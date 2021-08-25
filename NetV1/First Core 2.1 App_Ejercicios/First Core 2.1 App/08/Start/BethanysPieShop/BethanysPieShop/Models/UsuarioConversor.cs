40c038440f776f1f37afd914a24fae2f25a7b94b
manuelhigueras <manuelhigueras29@gmail.com>
2021-08-25 17:23:49 +0200

    NetV1 part 4

---
+++ NetV1/First Core 2.1 App_Ejercicios/First Core 2.1 App/08/Start/BethanysPieShop/BethanysPieShop/Models/UsuarioConversor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace BethanysPieShop.Models
+{
+    //SE IMPLEMENTA EL IdentityUser para la autenticacion y user
+
+
+    public class UsuarioConversor : IdentityUser
+    {
+        public DateTime FechaNacimiento { get; set; }
+
+        public int IdPais { get; set;}
+    }
+}
