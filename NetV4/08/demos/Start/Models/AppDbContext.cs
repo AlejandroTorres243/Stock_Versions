d0df98fbd2b493a7c04571ab08508fdf6a3ce2c8
manuelhigueras <manuelhigueras29@gmail.com>
2021-08-25 19:27:15 +0200

    Update NetV4

---
+++ NetV4/08/demos/Start/Models/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieShop.Models
+{
+
+    //PROCESO PARA CREAR SEGURIDAD EN LA APLICACION
+
+    public class AppDbContext: IdentityDbContext //BASE DE DATOS QUE EN NUESTRO CASO SERIA DE MONEDAS 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Pie> Pies { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+    }
+}
